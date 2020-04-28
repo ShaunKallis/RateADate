73881b938972c5f4272e271ea5d0b7f8ac05a2cf
ethanccastro <ec2-user@ip-172-31-14-134.us-east-2.compute.internal>
2020-04-28 16:50:39 +0000

    Created review page

--- app.js
+++ app.js
@@ -308,7 +308,7 @@ async function addToCart(username, pokemonName, quantityChosen) {
         });
 }
 
-async function createUser(username, password, email, bio) {
+async function createUser(username, password, email, bio, reviews) {
     console.log(`createUser called`);
     let result = await client.db("RateADate").collection("users").findOne({ "username": username });
     if (result != null) {
@@ -368,6 +368,55 @@ async function clearCart(username) {
     result = await client.db(database_name).collection("users").updateOne({ "username": username }, { $unset: { cart: null } });
 }
 
+async function addReview(username, rating, textReview) {
+    var result = await client.db("RateADate").collection("users").findOne({ "username": username });
+    console.log(result);
+
+
+        result = await client.db("RateADate").collection("users").updateOne(
+                        {username: username},
+                        { $set:
+                            {
+                            reviews: {
+                                starRating: rating,
+                                textRating: textReview
+                            }}
+                        });
+        
+
+    // else {
+    //     var index;
+    //     var found = false;
+    //     for (index = 0; index < result.cart.length; index++) {
+    //         if (result.cart[index][0] == pokemonName) {
+    //             found = true;
+    //             break;
+    //         }
+    //     }
+    //     if (found) {
+    //         result.cart[index][2] = quantityChosen;
+    //     }
+    //     else {
+    //         result.cart[result.cart.length] = [pokemonName, pokemon.price, quantityChosen];
+    //     }
+    // }
+    // console.log(`new cart: `);
+    // var index;
+    // for (index = 0; index < result.cart.length; index++) {
+    //     console.log(result.cart[index]);
+    // }
+    // result = await client.db(database_name).collection("users").updateOne(
+    //     { "username": username },
+    //     {
+    //         $set: { "cart": result.cart }
+    //     });
+}
+
+async function getReviews(username) {
+    const result = await client.db("RateADate").collection("users").find({ "name": username }).toArray();
+    return result;
+}
+
 app.get("/searchProduct", isUserAuthenticated, async function (req, res) {
 
     //   let categories = await getCategories();
@@ -425,6 +474,31 @@ app.get("/profilePage", async function (req, res) {
     console.log(userProf);
     res.render("profilePage", { "userProf": userProf })
 })
+
+//Route for User Profile Page
+app.get("/reviews", async function (req, res) {
+    //var cUser = req.cookie.cookieUser;
+    var cUser = req.session.name;
+    console.log("cUser: ", cUser);
+    let rows = await getReviews(cUser);
+    console.log("rows: ", rows);
+    const userProf = await client.db("RateADate").collection("users").findOne({ username: cUser });
+    console.log("userProf: ", userProf);
+    res.render("reviews", { "userProf": userProf,
+                            "records": rows})
+})
+
+app.post("/addreview", isUserAuthenticated, async function (req, res) {
+    const newReview = req.body;
+    console.log("review: ", newReview);
+    let rows = await(addReview(newReview.username, newReview.rating, newReview.textReview));
+    // const result = await insertProduct(newPokemon);
+    // console.log(`Pokemon added: ${result}`);
+    // if (!result) {
+    //     console.log("Pokemon's name: " + newPokemon.name);
+    //     return res.redirect("updateProduct?pokemonName=" + newPokemon.name);
+    // }
+});
 
 //Create Account POST
 app.post("/index", function (req, res) {
